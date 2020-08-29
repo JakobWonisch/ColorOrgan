db49b2fc333931765743804571e1c6f336d6ccd6
Hasi08 <martin.hasun@hotmail.com>
2020-08-29 12:36:51 +0200

    Add Sustain Button Model

--- Assets/Scripts/SustainSwitch.cs
+++ Assets/Scripts/SustainSwitch.cs
@@ -7,6 +7,9 @@ public class SustainSwitch : MonoBehaviour
 
     public GameObject endTopPressed;
 
+    public Material bOn, bOff, lOn, lOff;
+    public MeshRenderer mrButton, mrLight;
+
     public Sampler sampler;
 
     private bool pressed = false;
@@ -15,7 +18,7 @@ public class SustainSwitch : MonoBehaviour
     void Start()
     {
 
-       endTopPressed.SetActive(pressed);
+       SetActive(pressed);
 
     }
 
@@ -32,9 +35,17 @@ public class SustainSwitch : MonoBehaviour
 
         pressed = !pressed;
 
-       endTopPressed.SetActive(pressed);
-       
+        SetActive(pressed);
+
         Debug.Log("Pressed: " + pressed);
         sampler.SetSustain(pressed);
+    }
+
+    private void SetActive(bool p)
+    {
+        endTopPressed.SetActive(p);
+
+        mrButton.material = p ? bOn : bOff;
+        mrLight.material = p ? lOn : lOff;
     }
 }
