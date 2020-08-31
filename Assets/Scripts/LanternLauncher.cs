c897f977822fb64f547b8c54cccb8c60062e517d
JakobWonisch <jakob.wonisch@gmail.com>
2020-08-31 10:20:40 +0200

    Fix Lanterns Getting Stuck Under Objects

--- Assets/Scripts/LanternLauncher.cs
+++ Assets/Scripts/LanternLauncher.cs
@@ -28,6 +28,7 @@ public class LanternLauncher : MonoBehaviour
     // notes starting from c1
     public void Launch(int n)
     {
+        float halfHeight = lanternPrefab.GetComponentInChildren<Renderer>().bounds.size.y / 2;
         
         // pick random point in spawn
         Vector3 rand = spawn.size;
@@ -36,6 +37,39 @@ public class LanternLauncher : MonoBehaviour
         rand += spawn.center;
 
         rand = mainCamera.TransformPoint(rand);
+
+        // check if there's anything above...
+        // if so, move above that point
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(rand - Vector3.up * halfHeight, Vector3.up);
+
+        float maxDistance = -1;
+        RaycastHit furthestHit = new RaycastHit();
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+
+            if (hit.distance > maxDistance)
+            {
+                furthestHit = hit;
+                maxDistance = hit.distance;
+            }
+        }
+
+        // Debug.Log("Hits: " + hits.Length + " distance " + maxDistance);
+
+        // move lantern
+        if(maxDistance >= 0)
+        {
+            Renderer r = furthestHit.transform.GetComponentInChildren<Renderer>();
+            
+            if (r != null)
+                maxDistance += r.bounds.size.y;
+
+            rand += Vector3.up * maxDistance;
+            // Debug.Log("y: " + furthestHit.transform.GetComponent<Renderer>().bounds.size.y);
+        }
 
         GameObject go = Instantiate(lanternPrefab, rand, Quaternion.identity, transform);
         // go.transform.GetChild(0).GetComponent<MeshRenderer>().material = materials[n % materials.Length];
